871b1cd6688ac383d461e234882fa6dd238af90e
root <komono.32@gmail.com>
2017-05-26 21:48:56 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,52 @@
-const restify = require('restify');
+const BROWSER_MODE = false;
 
-let count = 0;
-function respond(req, res, next) {
-  res.send('[' + (count++) + '] hello ' + req.params.name);
-}
+
+const restify = require('restify');
+const mazeBot = require('./mazeBot');
 
 const server = restify.createServer();
-server.get('/hello/:name', respond);
+server.use(restify.bodyParser());
 
-server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
-});
+
+function _onName(req, res, next) {
+  res.status(200);
+  res.contentType = 'json';
+
+  const name = "egueto-" + (new Date()).getTime();
+  const response = {
+    name: name,
+    email: "edgar.gueto@ext.privalia.com"
+  };
+  res.send(response);
+}
+
+
+function _onMove(req, res, next) {
+  res.status(200);
+  res.contentType = 'json';
+
+  let moveResult = "right";
+  try {
+    const params = req.body || {};
+
+    params.game = params.game || {};
+    params.player = params.player || {};
+    params.maze = params.maze || {};
+    params.ghosts = params.ghosts || {};
+
+    moveResult = mazeBot.processNextMovement(params);
+  } catch(e) {
+    console.error("EXCEPTION", e);
+  }
+  const response = {
+    move: moveResult
+  };
+  res.send(response);
+}
+
+
+
+server.post('/name', _onName);
+server.post('/move', _onMove);
+
+server.listen(90, function () {});
